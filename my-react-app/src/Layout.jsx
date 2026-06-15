2faab27d0e38bd1b2f79a165c866ac0614dc2113
peter@ledovia.com <peter@ledovia.com>
2026-06-15 12:44:11 -0400

    Cleaned up Layout.jsx

--- my-react-app/src/Layout.jsx
+++ my-react-app/src/Layout.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
-import { Typography, Drawer, List, Toolbar, AppBar, ListItem, ListItemButton, ListItemIcon, ListItemText, Box, CssBaseline, IconButton } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import SettingsIcon from '@mui/icons-material/Settings';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Outlet  } from 'react-router-dom';
+import { Box } from '@mui/material';
 import LeftDrawer from './LeftDrawer';
 
 const drawerWidth = 240;
-
-const menuItems = [
-  /*{ text: 'Home', icon: <HomeIcon />, path: '/home' },*/
-  { text: 'Movie List', icon: <InfoIcon />, path: '/' },
-  { text: 'Admin Area', icon: <SettingsIcon />, path: '/AdminArea' },
-];
-
+const containerStyle = {
+     display: 'flex'
+}
 
 export default function Layout() {
   return (
-    <Box sx={{ display: 'flex' }}> 
+    <Box sx={containerStyle}> 
      <LeftDrawer drawerWidth={drawerWidth}/>
       <Box component="main" sx={{ flexGrow: 1, p: 5, width: `calc(100% - ${drawerWidth}px)` }}>
         <Outlet />
